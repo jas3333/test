01d140a6dc00ab2d9728f4cd3eb7a4d9f3c77b07
jas3333 <accounts1@protonmail.com>
2021-10-27 12:36:45 -0700

    dotfiles

---
+++ arch_install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+timedatectl set-ntp true
+
+lsblk
+
+echo "Enter disk to setup"
+read DISK
+
+sgdisk -Z ${DISK} # Remove everything on disk
+
+sgdisk -a 2048 -o ${DISK}
+
+sgdisk -n 1:0:+500M ${DISK}
+sgdisk -n 2:0:0     ${DISK}
+
+sgdisk -t 1:ef00 ${DISK}
+sgdisk -t 2:8300 ${DISK}
+
+sgdisk -c 1:"UEFISYS" ${DISK}
+sgdisk -c 2:"ROOT" ${DISK}
+
+mkfs.vfat -F32 "UEFISYS" "${DISK}p1"
+mkfs.ext4 "ROOT" "${DISK}p2"
