2d25a1e0e9f0e29d7b74a8a2722f259389c28cb6
jas3333 <accounts1@protonmail.com>
2021-10-27 15:58:22 -0700

    test

--- arch_install.sh
+++ arch_install.sh
@@ -25,10 +25,6 @@ case $continue in
 
     mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
     mkfs.ext4 -L "ROOT" "${DISK}2"
-    ;;
-*)
-    reboot now
-    ;;
 esac
 
 mount -L ROOT /mnt
@@ -36,7 +32,21 @@ mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount -L UEFISYS /mnt/boot 
 
-pacstrap /mnt base base-devel linux linux-firmware vim --noconfirm --needed
+pacstrap /mnt base linux linux-firmware vim --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1     localhost" >> /etc/hosts
+echo "::1           localhost" >> /etc/hosts
+echo "127.0.1.1     arch.localdomain    arch" >> /etc/hosts
+
 
 
