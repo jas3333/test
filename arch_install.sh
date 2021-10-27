73add418de748b9c3a5d0eb95a0ed2f37a09fbf1
jas3333 <accounts1@protonmail.com>
2021-10-27 13:26:12 -0700

    test

--- arch_install.sh
+++ arch_install.sh
@@ -9,19 +9,29 @@ read DISK
 read -p "This will erase this disk, are you sure?(y to continue otherwise exit)" continue
 
 case $continue in
-y|yes
-sgdisk -Z ${DISK} # Remove everything on disk
+    y|yes
+    sgdisk -Z ${DISK} # Remove everything on disk
 
-sgdisk -a 2048 -o ${DISK}
+    sgdisk -a 2048 -o ${DISK}
 
-sgdisk -n 1:0:+500M ${DISK}
-sgdisk -n 2:0:0     ${DISK}
+    sgdisk -n 1:0:+500M ${DISK}
+    sgdisk -n 2:0:0     ${DISK}
 
-sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+    sgdisk -t 1:ef00 ${DISK}
+    sgdisk -t 2:8300 ${DISK}
 
-sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"ROOT" ${DISK}
+    sgdisk -c 1:"UEFISYS" ${DISK}
+    sgdisk -c 2:"ROOT" ${DISK}
 
-mkfs.vfat -F32 "UEFISYS" "${DISK}1"
-mkfs.ext4 "ROOT" "${DISK}2"
+    mkfs.vfat -F32 "UEFISYS" "${DISK}1"
+    mkfs.ext4 "ROOT" "${DISK}2"
+
+    mount -L ROOT /mnt
+    mkdir /mnt/boot
+    mkdir /mnt/boot/efi
+    mount -L UEFISYS /mnt/boot 
+
+
+
+
+esac
