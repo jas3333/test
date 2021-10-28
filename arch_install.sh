3fc132cb9724ab317a849e0682e775bd095b4355
jas3333 <accounts1@protonmail.com>
2021-10-27 18:27:44 -0700

    test

--- arch_install.sh
+++ arch_install.sh
@@ -28,6 +28,7 @@ case $continue in
 esac
 
 mount -L ROOT /mnt
+mkdir /boot
 mkdir /boot/efi
 mount -L UEFISYS /boot/efi 
 
@@ -47,14 +48,14 @@ echo "127.0.0.1     localhost" >> /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     arch.localdomain    arch" >> /etc/hosts
 pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools openssh base-devel linux-headers dialog os-prober mtools dosfstools git kitty --noconfirm --needed
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 echo "Enter user name: "
 read NUSER
 
-useradd -m -G wheel {$NUSER}
-passwd {$NUSER}
+useradd -m -G wheel $NUSER
+passwd $NUSER
 echo "%wheel ALL=(ALL)" << /etc/sudoers
 EOF
 
