abe12a97073d24a707fca0e85288b52d33caf4da
jas3333 <accounts1@protonmail.com>
2021-10-27 20:01:32 -0700

    test

--- arch_install.sh
+++ arch_install.sh
@@ -32,49 +32,5 @@ mount -L ROOT /mnt
 pacstrap /mnt base linux linux-firmware vim --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
 mkdir /mnt/test
-cp -R /test /mnt/test
-arch-chroot /mnt /bin/bash << EOF
-
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-echo "arch" >> /etc/hostname
-echo "127.0.0.1     localhost" >> /etc/hosts
-echo "::1           localhost" >> /etc/hosts
-echo "127.0.1.1     arch.localdomain    arch" >> /etc/hosts
-
-cd /
-mkdir /boot/EFI
-mount -L UEFISYS /boot/EFI 
-
-pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools openssh base-devel linux-headers dialog os-prober mtools dosfstools git kitty --noconfirm --needed
-
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-
-read -p "Enter root password: " rpass
-read -p "Enter username: " username
-read -p "Enter user password: " upass
-
-echo "Setting root password."
-echo -en "$rpass\n$rpass" | passwd
-
-echo "Creating new user."
-useradd -m -G wheel $username
-echo -en "$upass\n$upass" | passwd $username
-
-echo "%wheel ALL=(ALL)" << /etc/sudoers
-EOF
-
-
-
-
-
-
+cp -r test /mnt/test
+arch-chroot /mnt 
--- arch_install2.sh
+++ arch_install2.sh
@@ -1,8 +1,6 @@
-#! /usr/bin/env bash
-
+#! /usr/bash
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
-
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 
 locale-gen
@@ -12,17 +10,24 @@ echo "127.0.0.1     localhost" >> /etc/hosts
 echo "::1           localhost" >> /etc/hosts
 echo "127.0.1.1     arch.localdomain    arch" >> /etc/hosts
 
-pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools openssh base-devel linux-headers dialog os-prober mtools dosfstools git kitty --noconfirm --needed
+pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools openssh base-devel linux-headers dialog os-prober mtools dosfstools git kitty sudo --noconfirm --needed
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+cd /
+mkdir /boot/EFI
+mount -L UEFISYS /boot/EFI
+
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable NetworkManager
+read -p "Enter root password: " rpass
+read -p "Enter username: " username
+read -p "Enter user password: " upass
 
-echo "Enter user name: "
-read USER
+echo "Setting root password."
+echo -en "$rpass\n$rpass" | passwd
 
-useradd -m -G wheel $USER
-passwd $USER
+echo "Creating new user."
+useradd -m -G wheel $username
+echo -en "$upass\n$upass" | passwd $username
 
-
+echo "%wheel ALL=(ALL)" << /etc/sudoers
