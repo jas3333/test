237f8e4c69287df7038a818f560a4f43383e1e63
jas3333 <accounts1@protonmail.com>
2021-10-27 17:48:10 -0700

    test

--- arch_install.sh
+++ arch_install.sh
@@ -34,8 +34,33 @@ mount -L UEFISYS /mnt/boot
 
 pacstrap /mnt base linux linux-firmware vim --noconfirm --needed
 genfstab -U /mnt >> /mnt/etc/fstab
+mkdir /mnt/root/test
 cp -R /test /mnt/root/test
-arch-chroot /mnt
+arch-chroot /mnt /bin/bash << EOF
+
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1     localhost" >> /etc/hosts
+echo "::1           localhost" >> /etc/hosts
+echo "127.0.1.1     arch.localdomain    arch" >> /etc/hosts
+pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools openssh base-devel linux-headers dialog os-prober mtools dosfstools git kitty --noconfirm --needed
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+echo "Enter user name: "
+read USER
+
+useradd -m -G wheel $USER
+passwd $USER
+echo "%wheel ALL=(ALL)" << /etc/sudoers
+EOF
+
+
+
 
 
 
